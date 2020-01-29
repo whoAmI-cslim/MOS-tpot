7c760fd6799c7657a7ce1ba6f28b1c08a603edc7
whoAmI-cslim <clyde.hunter1984@gmail.com>
2020-01-28 19:06:27 -0500

    I'm a tard

---
+++ test2.py
@@ -0,0 +1,23 @@
+import urllib.request
+import json
+import sys
+import os
+import datetime
+
+symbols = ['MSFT', 'BABA']
+
+n = 2
+
+if n > 1:
+ x = 0
+ while n > 1 and n < 5:
+  n + 1
+  x + 1
+  var = "var{}".format(x)
+  var = symbols
+  print (var)
+
+
+# var1, var2  = symbols
+# print (var1)
+# print (var2)
